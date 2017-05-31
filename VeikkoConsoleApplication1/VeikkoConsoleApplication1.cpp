d2443b6f4f5c44ccd60f0cc08e033d18b48c35ac
Veikko Juusola <veikko.juusola@m-files.com>
2017-05-31 08:43:59 +0300

    push

---
+++ VeikkoConsoleApplication1/VeikkoConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+// VeikkoConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("Kukkuu \n");
+		printf("taas!\n\n");
+		
+	}
+
+    return 0;
+}
+
